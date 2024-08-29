cd9f9737a628b1d15603a471f918a10db6926a60
Javion <javionlog@163.com>
2024-08-29 10:40:27 +0800

    fix: remove authPlugin

--- src/modules/auth/plugins/index.ts
+++ src/modules/auth/plugins/index.ts
@@ -7,34 +7,9 @@ export const jwtPlugin = (app: Elysia) => {
   return app.use(jwt({ name: JWT_NAME, secret: JWT_SECRET }))
 }
 
-export const authPlugin = (app: Elysia) => {
-  return app.use(jwtPlugin).guard({
-    async beforeHandle({ jwt, set, cookie: { accessToken } }) {
-      if (!accessToken?.value) {
-        set.status = 'Unauthorized'
-        throw new Error('Access token is missing')
-      }
-      const jwtPayload = await jwt.verify(accessToken.value)
-      if (!jwtPayload) {
-        set.status = 'Forbidden'
-        throw new Error('Access token is invalid')
-      }
-      if (!jwtPayload.sub) {
-        set.status = 'Forbidden'
-        throw new Error('Access token is invalid')
-      }
-      const user = await getUserByUsername({ username: jwtPayload.sub })
-      if (!user) {
-        set.status = 'Forbidden'
-        throw new Error('Access token is invalid')
-      }
-    }
-  })
-}
-
 export const authResolve = (app: Elysia) => {
   return app
-    .use(jwt({ name: JWT_NAME, secret: JWT_SECRET }))
+    .use(jwtPlugin)
     .resolve(async ({ jwt, set, cookie: { accessToken } }) => {
       if (!accessToken?.value) {
         set.status = 'Unauthorized'
--- src/modules/shared/controllers/index.ts
+++ src/modules/shared/controllers/index.ts
@@ -1,6 +1,6 @@
-import { authPlugin, jwtPlugin } from '@/modules/auth/plugins/index'
+import { authResolve, jwtPlugin } from '@/modules/auth/plugins/index'
 import { Elysia } from 'elysia'
 
 export const BaseController = new Elysia().use(jwtPlugin)
 
-export const GuardController = new Elysia().use(authPlugin)
+export const GuardController = new Elysia().use(authResolve)
